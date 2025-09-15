d7d704f5689708e17363119fe6afde0cf0dcd9a6
misna04 <misnasyari@gmail.com>
2025-09-15 14:22:20 +0800

    route basepath

--- next.config.ts
+++ next.config.ts
@@ -3,12 +3,7 @@
 const repoName = "https://github.com/mshuecodev/authentication-firebase.git" // replace with your actual repo name
 
 const nextConfig = {
-	output: "export",
-	images: {
-		unoptimized: true
-	},
-	basePath: process.env.NODE_ENV === "production" ? `/${repoName}` : "",
-	assetPrefix: process.env.NODE_ENV === "production" ? `/${repoName}/` : ""
+	basePath: "/authentication-firebase" //production
 }
 
 module.exports = nextConfig
